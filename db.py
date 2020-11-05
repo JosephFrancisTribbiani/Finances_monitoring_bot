9b9bfe8e9de33477c23f2451026c49d6d4a60e29
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2020-11-05 21:45:01 +0300

    test

--- db.py
+++ db.py
@@ -1,14 +1,14 @@
-import psycopg2
-from config import DB_LINK
-
-__connection = None
-
-
-def get_connection():
-    global __connection
-    if __connection is None:
-        __connection = psycopg2.connect(DB_LINK, sslmode='require')
-    return __connection
+# import psycopg2
+# from config import DB_LINK
+#
+# __connection = None
+#
+#
+# def get_connection():
+#     global __connection
+#     if __connection is None:
+#         __connection = psycopg2.connect(DB_LINK, sslmode='require')
+#     return __connection
 
 #
 # def init_db(force: bool = False):
@@ -24,17 +24,17 @@ def get_connection():
 #                 ''')
 
 
-def test(msg):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    user = 3
-    fname = msg.chat.first_name
-    lname = msg.chat.last_name
-    nick = 'Kisonka'
-
-    cur.execute(f'''
-                INSERT INTO users(user_id, fname, lname, nickname)
-                VALUES (?, ?, ?, ?);
-                ''', (user, fname, lname, nick))
-    conn.commit()
+# def test(msg):
+#     conn = get_connection()
+#     cur = conn.cursor()
+#
+#     user = 3
+#     fname = msg.chat.first_name
+#     lname = msg.chat.last_name
+#     nick = 'Kisonka'
+#
+#     cur.execute(f'''
+#                 INSERT INTO users(user_id, fname, lname, nickname)
+#                 VALUES (?, ?, ?, ?);
+#                 ''', (user, fname, lname, nick))
+#     conn.commit()
