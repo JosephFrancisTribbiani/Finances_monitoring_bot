6619e6c614b4a9872b7706cb66f0675fdd1ff279
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2020-11-05 21:17:25 +0300

    Test

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-TOKEN = "1474084336:AAEAU5UFYEBctyhkmVWNW5N9QTS8sN8IAtE"
+import os
+
+TOKEN = '1474084336:AAEAU5UFYEBctyhkmVWNW5N9QTS8sN8IAtE'
+DATABASE_URL = 'postgres://bvzqiaqdotxmbs:46f133b16a9f69bb5fd3ee6e0bdbbe9b0dba86f9049ffecf17a0e58610849dea@ec2-46-137-124-19.eu-west-1.compute.amazonaws.com:5432/d8hjk48q7mt2id'
+DB_LINK = os.environ[DATABASE_URL]
---
+++ db.py
@@ -0,0 +1,38 @@
+import psycopg2
+from config import DB_LINK
+
+__connection = None
+
+
+def get_connection():
+    global __connection
+    if __connection is None:
+        __connection = psycopg2.connect(DB_LINK, sslmode='require')
+    return __connection
+
+#
+# def init_db(force: bool = False):
+#     conn = get_connection()
+#     cur = conn.cursor()
+#
+#     if force:
+#         cur.execute('DROP TABLE IF EXISTS expenses')
+#
+#     cur.execute('''CREATE TABLE IF NOT EXISTS expenses(
+#                 id SERIAL,
+#                 chat_id INTEGER NOT NULL
+#                 ''')
+
+
+def test(msg):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    user = 3
+    fname = msg.chat.first_name
+    lname = msg.chat.last_name
+
+    cur.execute(f'''
+                INSERT INTO users(user_id, fname, lname, nickname)
+                VALUES ({user}, {fname}, {lname}, NULL);
+                ''')
