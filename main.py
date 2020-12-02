2c59a32431182b5c4ed10dcc19486e95673a19df
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2020-12-02 23:25:54 +0300

    Updated

--- main.py
+++ main.py
@@ -322,6 +322,7 @@ def webhook():
 
 def main():
     init_db(force=False)
+    server.run(host="0.0.0.0", port=PORT)
 
 
 def record_confirm(msg_id):
@@ -341,5 +342,4 @@ def record_confirm(msg_id):
 
 
 if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=PORT)
     main()
